f9deb403f508d5e4d1ad8e9a364280b7b72679a4
YaEhley <143018286+YaEhley@users.noreply.github.com>
2023-12-24 16:02:03 +0300

    ver.2

--- js/task-4.js
+++ js/task-4.js
@@ -1,3 +1,7 @@
+const getTotalBalanceByGender = (users, gender) => users
+    .filter(user => user.gender === gender)
+    .reduce((totalBalance, user) => totalBalance + user.balance, 0);
+ 
 const allUsers = [
     {
     name: "Moore Hensley",
@@ -36,10 +40,6 @@ const allUsers = [
   }
 ];
 
-const getTotalBalanceByGender = (users, gender) => users
-    .filter(user => user.gender === gender)
-    .reduce((totalBalance, user) => totalBalance + user.balance, 0);
- 
 console.log(getTotalBalanceByGender(allUsers, "male")); // 12053
 
 console.log(getTotalBalanceByGender(allUsers, "female")); // 8863
